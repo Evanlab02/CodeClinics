0bf7dc7af1225d6664dd610e4067591468614a3a
evanlab02 <evanlabuschagne70@gmail.com>
2022-08-10 14:08:19 +0200

    Displays if a user is already logged in

--- api/token.py
+++ api/token.py
@@ -2,8 +2,11 @@
 Houses all logic in regards to the token.json file
 """
 
+from os import system
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 from output.my_output import neat_print
+from installation.file_path_helper import path_exists
 
 def create_token(settings: dict):
     """Creates a token (token.json) using oauth to login
@@ -24,3 +27,47 @@ def create_token(settings: dict):
     with open(f'{storage_directory}token.json', 'w', encoding="utf-8") as token:
         token.write(creds.to_json())
     neat_print("[green]Token created![/green]")
+
+
+def load_token(settings: dict):
+    """
+    Will load the token off of the token.json file, if
+    there is nothing to load, it will return None
+    """
+    storage_directory = settings["STORAGE PATH"]
+    permission = settings["PERMISSIONS"]
+    creds = None
+    if path_exists(f'{storage_directory}token.json'):
+        creds = Credentials.from_authorized_user_file(f'{storage_directory}token.json', permission)
+    return creds
+
+
+def token_valid(creds):
+    """
+    Checks that the token exists and is valid
+    """
+    return creds and creds.valid
+
+
+def token_expired(creds):
+    """
+    Checks that the token exists and is expired or needs to be refreshed
+    """
+    return creds and creds.expired and creds.refresh_token
+
+
+def check_token(creds):
+    """
+    Checks that the token is valid and not expired, if it
+    meets both these conditions will return True, otherwise False
+    """
+    return token_valid(creds) and not token_expired(creds)
+
+
+def remove_token(storage_directory: str):
+    """Will delete the token.json at the storage directory"""
+    token_exists = False
+    if path_exists(f"{storage_directory}token.json"):
+        system(f"rm {storage_directory}token.json")
+        token_exists = True
+    return token_exists
--- commands/login_command.py
+++ commands/login_command.py
@@ -1,6 +1,6 @@
 """Houses all logic to login a user in with their gmail account"""
 
-from api.token import create_token
+from api.token import create_token, load_token, check_token
 from output.my_output import neat_print
 
 def do_login(settings: dict):
@@ -8,5 +8,10 @@ def do_login(settings: dict):
     Login Command - Logs into the code clinic
     """
     neat_print("[magenta]Logging in...[/magenta]")
-    create_token(settings)
-    neat_print("[green]Logged in![/green]")
+    creds = load_token(settings)
+
+    if check_token(creds):
+        neat_print("[green]a User is logged in[/green]")
+    else:
+        create_token(settings)
+        neat_print("[green]Logged in![/green]")
