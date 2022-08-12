4162a07ceb5e38667ed8eb1c5e46da87977499b0
evanlab02 <evanlabuschagne70@gmail.com>
2022-08-12 12:04:41 +0200

    Test Update

--- testing/unittests/test_installation.py
+++ testing/unittests/test_installation.py
@@ -10,13 +10,16 @@ from os import system
 from os.path import expanduser, exists
 from unittest.mock import patch
 from io import StringIO
-from installation import file_path_helper
+from installation import (
+    file_path_helper, 
+    calendar_id, 
+    continue_install,
+    describe_install)
 
 class MyTestCase(unittest.TestCase):
     """
     Just a class to contain tests
     """
-
 
     @patch("sys.stdout", StringIO())
     def test_get_home_dir(self):
@@ -51,3 +54,42 @@ class MyTestCase(unittest.TestCase):
         file_path_helper.create_directory("fisher/")
         self.assertTrue(exists("fisher/"))
         system("rm -rf fisher/")
+
+    @patch("sys.stdin", StringIO("ABCD\n\n"))
+    @patch("sys.stdout", StringIO())
+    def test_get_calendar_id(self):
+        """Tests getting calendar id"""
+        expected = "ABCD"
+        self.assertEqual(expected, calendar_id.get_calendar_id())
+
+
+    @patch("sys.stdout", StringIO())
+    def test_load_settings(self):
+        """Tests loading settings"""
+        expected = {"SETTINGS": "THIS IS A TEST"}
+        self.assertEqual(expected, continue_install.load_settings_on_disk("testing/unittests/resources/settings.json"))
+
+
+    @patch("sys.stdout", StringIO())
+    def test_load_settings_invalid(self):
+        """Tests loading settings"""
+        try:
+            expected = {"SETTINGS": "THIS IS A TEST"}
+            self.assertEqual(expected, continue_install.load_settings_on_disk("testing/unittests/settings.json"))
+        except FileNotFoundError:
+            print("TEST PASSED")
+
+    @patch("sys.stdin", StringIO("\n"))
+    @patch("sys.stdout", StringIO())
+    def test_read_file_checks_valid(self):
+        """Tests reading file"""
+        mock_value = {"EXISTS": "Yes"}
+        self.assertTrue(describe_install.read_file_checks(mock_value))
+
+
+    @patch("sys.stdin", StringIO("\n"))
+    @patch("sys.stdout", StringIO())
+    def test_read_file_checks_invalid(self):
+        """Tests reading file"""
+        mock_value = {"EXISTS": "No"}
+        self.assertFalse(describe_install.read_file_checks(mock_value))
