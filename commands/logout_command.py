c0b498ae713e8f6c6354e404381f58422394cf5d
evanlab02 <evanlabuschagne70@gmail.com>
2022-08-12 11:16:46 +0200

    Removed trailing whitespaces

--- commands/logout_command.py
+++ commands/logout_command.py
@@ -15,9 +15,8 @@ def do_logout(settings: dict):
 
     storage_directory = settings["STORAGE PATH"]
     removed_token = remove_token(storage_directory)
-    
+
     if removed_token:
         neat_print("[green]Logged out successfully[/green]")
     else:
         neat_print("[yellow]No user to logout[/yellow]")
-    
