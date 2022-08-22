3fc4474e33d22ebc149f2b1b64e773ce6b6a22e7
Evan Labuschagne <36800222+Evanlab02@users.noreply.github.com>
2022-08-22 11:47:08 +0200

    Added Calendar Displaying (#58)

--- output/rich_output.py
+++ output/rich_output.py
@@ -1,8 +1,12 @@
 """Module responsible for all output"""
 
+from datetime import datetime
 from rich.console import Console
 from rich.table import Table
 from rich import print as rprint
+
+# File helpers
+from file_helpers.csv_helper import generate_csv_row
 
 def display_basic_table(output: dict):
     """Displays basic table based on dictionary received"""
@@ -25,3 +29,47 @@ def display_basic_table(output: dict):
 def neat_print(message: str):
     """Prints message using rich print to add extra formatting when required"""
     rprint(message)
+
+
+def calendar_print(events: list, starting_time="NA"):
+    """Displays a table of events based on the events list"""
+    header_style = "bold magenta"
+    header_enabled = True
+    footer_enabled = False
+    columns = ["ID", "Title", "Date", "Start Time", "End Time", "Description", "Hangout Link"]
+    rows = []
+
+    for event in events:
+        rows.append(generate_csv_row(event))
+
+    console = Console()
+    table = Table(show_header=header_enabled, show_footer=footer_enabled, header_style=header_style)
+    table.title = "Calendar Events"
+
+    for column in columns:
+        table.add_column(column)
+
+    table.columns[0].header_style = "bold blue"
+    table.columns[0].style = "blue"
+    table.columns[1].header_style = "bold magenta"
+    table.columns[1].style = "magenta"
+    table.columns[2].header_style = "bold cyan"
+    table.columns[2].style = "cyan"
+    table.columns[3].header_style = "bold cyan"
+    table.columns[3].style = "cyan"
+    table.columns[4].header_style = "bold cyan"
+    table.columns[4].style = "cyan"
+    table.columns[5].header_style = "bold yellow"
+    table.columns[5].style = "yellow"
+    table.columns[6].header_style = "bold green"
+    table.columns[6].style = "green"
+
+    if starting_time != "NA":
+        query_time = datetime.now()-starting_time
+        table.caption = f"Query Time: {query_time}"
+
+    for row in rows:
+        table.add_row(*row)
+
+    console.print(table)
+    return table
