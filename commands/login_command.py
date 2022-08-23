235484095464b73629ed05d45875150b2de15710
Evan Labuschagne <evanlabuschagne70@gmail.com>
2022-08-23 22:19:15 +0200

    Added manual email login and logout

--- commands/login_command.py
+++ commands/login_command.py
@@ -1,6 +1,7 @@
 """Houses all logic to login a user in with their gmail account"""
 
 from google_calendar_API.token_helper import create_token, load_token, check_token
+from file_helpers.json_helper import overwrite_json_file
 from output.rich_output import neat_print
 
 def do_login(settings: dict):
@@ -12,8 +13,12 @@ def do_login(settings: dict):
     neat_print("[magenta]Logging in...[/magenta]")
     creds = load_token(storage_path, permission)
 
+    settings["USER EMAIL"] = input("Enter your email: ")
+
     if check_token(creds):
         neat_print("[green]a User is logged in[/green]")
     else:
         create_token(storage_path, permission)
         neat_print("[green]Logged in![/green]")
+
+    overwrite_json_file(f"{storage_path}settings.json", settings)
--- commands/logout_command.py
+++ commands/logout_command.py
@@ -4,6 +4,7 @@ Houses all logic to logout the user
 
 # From Import Statements
 from output.rich_output import neat_print
+from file_helpers.json_helper import overwrite_json_file
 from google_calendar_API.token_helper import remove_token
 
 
@@ -16,7 +17,11 @@ def do_logout(settings: dict):
     storage_directory = settings["STORAGE PATH"]
     removed_token = remove_token(storage_directory)
 
+    settings["USER EMAIL"] = "[NO EMAIL]"
+
     if removed_token:
         neat_print("[green]Logged out successfully[/green]")
     else:
         neat_print("[yellow]No user to logout[/yellow]")
+
+    overwrite_json_file(f"{storage_directory}settings.json", settings)
