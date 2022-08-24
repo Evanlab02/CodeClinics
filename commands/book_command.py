a4b6b2c953aa7fd65082de4f3865f9242fcb693f
Evan Labuschagne <36800222+Evanlab02@users.noreply.github.com>
2022-08-24 13:52:43 +0200

    Double attendance will now be prohibited  (#75)
    * Double attendance will now be prohibited
    
    * Local events will now be updated after book command

--- commands/book_command.py
+++ commands/book_command.py
@@ -2,19 +2,31 @@
 Houses all the functions used to book a code clinic session
 """
 
-from commands.command_helper import create_connection, get_events, get_event_specifics
+from commands.command_helper import (
+    create_connection,
+    get_all_events,
+    get_events,
+    get_event_specifics,
+    filter_events_to_open_events_only
+)
 from google_calendar_API.event_helper import attend_event
 
 from input_helpers.inquirer_helper import get_selected_event
+
+from storage.calendar_saving import save_events
 
 def do_booking(settings: dict):
     """
     This function is used to book a code clinic session
     """
     calendar_id = settings['CALENDAR ID']
+    storage_path = settings['STORAGE PATH']
     connection = create_connection(settings)
     events = get_events(connection, calendar_id)
+    events = filter_events_to_open_events_only(events)
     selected_event_formatted = get_selected_event(events)
     selected_id = selected_event_formatted.split(" - ")[0]
     event = get_event_specifics(events, selected_id)
     attend_event(connection, calendar_id, event)
+    events = get_all_events(connection, calendar_id)
+    save_events(storage_path, events)
--- commands/command_helper.py
+++ commands/command_helper.py
@@ -4,7 +4,7 @@ Houses functions that are commonly used accross the commands package.
 
 from google_calendar_API.date_helper import get_dates
 from google_calendar_API.connection_helper import create_api_connection
-from google_calendar_API.download_helper import download_multi_events
+from google_calendar_API.download_helper import download_events, download_multi_events
 from google_calendar_API.token_helper import load_token, validate_token
 
 
@@ -38,5 +38,25 @@ def get_events(connection, calendar_id: str):
     """
     dates = get_dates()
 
+    events = download_events(connection, dates, calendar_id)
+    return events
+
+
+def filter_events_to_open_events_only(events: list):
+    """
+    Filters the events to only open events.
+    """
+    open_events = []
+    for event in events:
+        if not len(event['attendees']) > 1:
+            open_events.append(event)
+    return open_events
+
+
+def get_all_events(connection, calendar_id: str):
+    """
+    Gets all events from the calendar.
+    """
+    dates = get_dates()
     events = download_multi_events(connection, dates, calendar_id)
     return events
